03ec487034a32a956538cf40bc33628fbc0483ce
Bryan Bergo <bryan.l.bergo@gmail.com>
2023-09-30 09:36:48 -0700

    typo fix

--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -1,7 +1,7 @@
 import { UserRepository } from "@repositories/index";
 
 /**
- * Service function for logging a user in/
+ * Service function for logging a user in
  * 
  * @param {string} username - the username to login with
  * @param {string} password - the password to login with
