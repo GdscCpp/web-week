86556491b9c46cd39b070d509412d24b11a0042f
Bryan Bergo <bryan.l.bergo@gmail.com>
2023-10-01 21:48:36 -0700

    ui fixes

--- frontend/src/app/upload/page.tsx
+++ frontend/src/app/upload/page.tsx
@@ -6,24 +6,21 @@ export default function Upload() {
 
   return (
     <main>
-      <div className={'flex flex-row h-fit'}>
-        <div className={'w-1/2 bg-neutral-800 h-fit '}>
-            <h1 className=" text-white text-[70px] font-semibold font-['Inter'] pl-[80px] mt-10">
+      <div className={'flex flex-row h-screen w-full'}>
+        <div className={'w-1/2 bg-neutral-800 pr-10 pl-10'}>
+            <h1 className=" text-white text-[40px] font-semibold font-['Inter'] pl-[80px] mt-10">
                 Upload your resume on the right!
             </h1>
-            <p className="ml-[80px] text-white text-2xl font-semibold font-['Inter'] leading-[40px] mt-20 mr-20">
+            <p className=" text-white text-2xl font-semibold font-['Inter'] leading-[40px] mt-5 ">
                 RateMyResume is a service that allows you to upload your resume, which will be reviewed by professionals,
                 allowing you to make improvements and submit the best version you can.
             </p>
-            <p className="ml-[80px] text-white text-2xl font-semibold font-['Inter'] leading-[40px] mt-20 mr-20">
-                To use the service, simply save your resume as a file on your computer (if you haven't already), and then
+            <p className=" text-white text-2xl font-semibold font-['Inter'] leading-[40px] mt-5">
+                To use the service, simply save your resume as a file on your computer (if you haven&apos;t already), and then
                 either drag and drop it to the space on the right, or click the area to open a file picker.
             </p>
-            <div className={"text-white text-5xl font-normal font-['Jockey One'] text-left mt-60 ml-20 mb-20"}>
+            <div className={"text-white text-5xl font-normal font-['Jockey One'] text-left fixed bottom-4"}>
                 <span>RateMyResume</span>
-            </div>
-            <div>
-
             </div>
         </div>
         <div className={'w-1/2 bg-neutral-700 flex justify-center '}>
